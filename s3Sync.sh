04572b9a540e3c2b1278494674e4bab59cc994d2
Allen Plummer <allen@73prime.io>
2022-01-08 15:49:32 -0600

    fixed base path

--- s3Sync.sh
+++ s3Sync.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [ -z $1 ]; then echo "You need to pass in a directory."; exit; fi
-
-aws s3 sync $1 s3://$S3_BUCKET \
+fullpath=$(readlink -f $1)
+echo Will build directory structure in s3 based on $fullpath
+aws s3 sync $1 s3://$S3_BUCKET/$1 \
             --exclude '*/.git/*' \
             --exclude '*/.terraform/*' \
             --exclude '*/.idea/*' \
